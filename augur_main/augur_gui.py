1396bc8ae0848447cf6da24b1132a7c98e016690
kevinbien <145506742+kevinbien@users.noreply.github.com>
2026-08-06 09:49:29 -0500

    Update augur_gui.py

--- augur_main/augur_gui.py
+++ augur_main/augur_gui.py
@@ -28,7 +28,7 @@ class AugurGUI(QWidget):
         super().__init__()
 
         # Set up the window
-        self.setWindowTitle("Augur 0.5")
+        self.setWindowTitle("Augur 0.8")
         width, height = get_monitors()[0].width // 4, get_monitors()[0].height // 3
         self.setGeometry(0, 0, width, height)
 
